32b8fe0f697cf949b93383e929c7d37c130d5c14
Ashwin-9726 <ashwinsvce15@gmail.com>
2025-02-24 15:33:20 +0530

    employee frontend

---
+++ ems-frontend/src/components/FooterComponenet.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FooterComponenet = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>
+                All rights reserved 2025 by Guvi
+            </span>
+
+        </footer>
+        
+        </div>
+  )
+}
+
+export default FooterComponenet
